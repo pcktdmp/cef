5680f74154dff72d6b739608ccdf898278139606
Serge van Namen <serge@se-cured.org>
2020-03-05 23:56:08 +0100

    add escape test

--- cefevent_test.go
+++ cefevent_test.go
@@ -5,11 +5,10 @@ import (
 	"testing"
 )
 
-func TestCefEvent(t *testing.T) {
+func TestCefEventExpected(t *testing.T) {
 
 	ext := make(map[string]string)
 	ext["sourceAddress"] = "127.0.0.1"
-	ext["requestClientApplication"] = "Go-http-client/1.1"
 
 	event := cefevent.CefEvent{
 		Version:            "0",
@@ -22,7 +21,32 @@ func TestCefEvent(t *testing.T) {
 		Extensions:         ext,
 	}
 
-	want := "CEF:0|Cool Vendor|Cool Product|1.0|COOL_THING|Something cool happened.|Unknown|sourceAddress=127.0.0.1 requestClientApplication=Go-http-client/1.1"
+	want := "CEF:0|Cool Vendor|Cool Product|1.0|COOL_THING|Something cool happened.|Unknown|sourceAddress=127.0.0.1"
+	got := event.Generate()
+
+	if want != got {
+		t.Errorf("event.Generate() = %q, want %q", got, want)
+	}
+
+}
+
+func TestCefEventEscape(t *testing.T) {
+
+	ext := make(map[string]string)
+	ext["sourceAddress\\"] = "\n127.0.0.1="
+
+	event := cefevent.CefEvent{
+		Version:            "0",
+		DeviceVendor:       "\\Cool\nVendor|",
+		DeviceProduct:      "Cool Product",
+		DeviceVersion:      "1.0",
+		DeviceEventClassId: "COOL_THING",
+		Name:               "Something cool happened.",
+		Severity:           "Unknown",
+		Extensions:         ext,
+	}
+
+	want := "CEF:0|\\\\Cool\\nVendor\\||Cool Product|1.0|COOL_THING|Something cool happened.|Unknown|sourceAddress\\\\=\\n127.0.0.1\\="
 	got := event.Generate()
 
 	if want != got {
