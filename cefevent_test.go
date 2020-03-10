aa390ea048a8f6759976379ac840bbba68352ab8
Serge van Namen <serge@se-cured.org>
2020-03-10 22:10:01 +0100

    add more test cases

--- cefevent_test.go
+++ cefevent_test.go
@@ -55,7 +55,7 @@ func TestCefEventEscape(t *testing.T) {
 
 }
 
-func TestCefEventMandatoryFields(t *testing.T) {
+func TestCefEventMandatoryVersionField(t *testing.T) {
 
 	event := cefevent.CefEvent{
 		Version:            "0",
@@ -67,11 +67,164 @@ func TestCefEventMandatoryFields(t *testing.T) {
 		Severity:           "Unknown",
 	}
 
-	noVersion := event
-	noVersion.Version = ""
-	_, err := noVersion.Generate()
+	brokenEvent := event
+	brokenEvent.Version = ""
+	_, err := brokenEvent.Generate()
 
 	if err == nil {
 		t.Errorf("%v", err)
+	}
+}
+
+func TestCefEventMandatoryDeviceVendorField(t *testing.T) {
+
+	event := cefevent.CefEvent{
+		Version:            "0",
+		DeviceVendor:       "Cool Vendor",
+		DeviceProduct:      "Cool Product",
+		DeviceVersion:      "1.0",
+		DeviceEventClassId: "COOL_THING",
+		Name:               "Something cool happened.",
+		Severity:           "Unknown",
+	}
+
+	brokenEvent := event
+	brokenEvent.DeviceVendor = ""
+	_, err := brokenEvent.Generate()
+
+	if err == nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func TestCefEventMandatoryDeviceProductField(t *testing.T) {
+
+	event := cefevent.CefEvent{
+		Version:            "0",
+		DeviceVendor:       "Cool Vendor",
+		DeviceProduct:      "Cool Product",
+		DeviceVersion:      "1.0",
+		DeviceEventClassId: "COOL_THING",
+		Name:               "Something cool happened.",
+		Severity:           "Unknown",
+	}
+
+	brokenEvent := event
+	brokenEvent.DeviceProduct = ""
+	_, err := brokenEvent.Generate()
+
+	if err == nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func TestCefEventMandatoryDeviceVersionField(t *testing.T) {
+
+	event := cefevent.CefEvent{
+		Version:            "0",
+		DeviceVendor:       "Cool Vendor",
+		DeviceProduct:      "Cool Product",
+		DeviceVersion:      "1.0",
+		DeviceEventClassId: "COOL_THING",
+		Name:               "Something cool happened.",
+		Severity:           "Unknown",
+	}
+
+	brokenEvent := event
+	brokenEvent.DeviceVersion = ""
+	_, err := brokenEvent.Generate()
+
+	if err == nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func TestCefEventMandatoryDeviceEventClassIdField(t *testing.T) {
+
+	event := cefevent.CefEvent{
+		Version:            "0",
+		DeviceVendor:       "Cool Vendor",
+		DeviceProduct:      "Cool Product",
+		DeviceVersion:      "1.0",
+		DeviceEventClassId: "COOL_THING",
+		Name:               "Something cool happened.",
+		Severity:           "Unknown",
+	}
+
+	brokenEvent := event
+	brokenEvent.DeviceEventClassId = ""
+	_, err := brokenEvent.Generate()
+
+	if err == nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func TestCefEventMandatoryNameField(t *testing.T) {
+
+	event := cefevent.CefEvent{
+		Version:            "0",
+		DeviceVendor:       "Cool Vendor",
+		DeviceProduct:      "Cool Product",
+		DeviceVersion:      "1.0",
+		DeviceEventClassId: "COOL_THING",
+		Name:               "Something cool happened.",
+		Severity:           "Unknown",
+	}
+
+	brokenEvent := event
+	brokenEvent.Name = ""
+	_, err := brokenEvent.Generate()
+
+	if err == nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func TestCefEventMandatorySeverityField(t *testing.T) {
+
+	event := cefevent.CefEvent{
+		Version:            "0",
+		DeviceVendor:       "Cool Vendor",
+		DeviceProduct:      "Cool Product",
+		DeviceVersion:      "1.0",
+		DeviceEventClassId: "COOL_THING",
+		Name:               "Something cool happened.",
+		Severity:           "Unknown",
+	}
+
+	brokenEvent := event
+	brokenEvent.Severity = ""
+	_, err := brokenEvent.Generate()
+
+	if err == nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func someImplementationOfCefEventer(e cefevent.CefEventer) bool {
+	return e.Validate()
+}
+
+func TestCefEventerValidate(t *testing.T) {
+
+	event := cefevent.CefEvent{
+		Version:            "0",
+		DeviceVendor:       "Cool Vendor",
+		DeviceProduct:      "Cool Product",
+		DeviceVersion:      "1.0",
+		DeviceEventClassId: "COOL_THING",
+		Name:               "Something cool happened.",
+		Severity:           "Unknown",
+	}
+
+	if !someImplementationOfCefEventer(&event) {
+		t.Errorf("Validation should be succesful here.")
+	}
+
+	noVersion := event
+	noVersion.Version = ""
+	if someImplementationOfCefEventer(&noVersion) {
+		t.Errorf("Validation should fail here.")
 	}
 }
